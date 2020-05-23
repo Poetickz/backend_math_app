cf660cf679dd20cebf8c1892216ef347b1ed9203
Alan Rocha <alan.rocha@udem.edu>
2020-05-23 03:05:40 -0500

    Added functionality

--- image_processing/process_image.py
+++ image_processing/process_image.py
@@ -4,36 +4,24 @@ import sys
 from knn import Knn
 
 knn = Knn()
-knn.set_k(20)
+knn.set_k(50)
 
-im = cv2.imread('4.png')
-im = cv2.resize(im,(28,28))
-image_array = im.reshape((1,784))
+im = cv2.imread('60.png')
+gray = cv2.cvtColor(im,cv2.COLOR_BGR2GRAY)
+# (2) threshold-inv and morph-open 
+th, threshed = cv2.threshold(gray, 100, 255, cv2.THRESH_OTSU|cv2.THRESH_BINARY_INV)
+morphed = cv2.morphologyEx(threshed, cv2.MORPH_OPEN, np.ones((2,2)))
+# (3) find and filter contours, then draw on src 
+cnts = cv2.findContours(morphed, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[-2]
 
-# cv2.imshow( "Display window", im )
-# cv2.waitKey(0)
-for i in image_array:
-  print(i, end=" ")
-print(len(image_array))
-print(knn.predict(image_array))
-
-
-# gray = cv2.cvtColor(im,cv2.COLOR_BGR2GRAY)
-# blur = cv2.GaussianBlur(gray, (5,5), 0)
-# thresh = cv2.adaptiveThreshold(blur,255,1,1,11,2)
-
-# out = np.zeros(im.shape,np.uint8)
-
-# # Finding Contours
-# contours,hierarchy = cv2.findContours(thresh,cv2.RETR_LIST,cv2.CHAIN_APPROX_SIMPLE)
-
-# for cnt in contours:
-#     if cv2.contourArea(cnt)>50:
-#         [x,y,w,h] = cv2.boundingRect(cnt)
-#         if  h>28:
-#             cv2.imshow( "Display window", im )
-#             waitKey(0);                                          
-#             roi = thresh[y:y+h,x:x+w]
-#             roismall = roismall.reshape((1,784))
-#             print(knn.predict(roismall[0]))
-
+for cnt in cnts:
+    x,y,w,h = bbox = cv2.boundingRect(cnt)
+    if  h>28:
+        cv2.rectangle(cnt, (x,y), (x+w, y+h), (255, 0, 255), 1, cv2.LINE_AA)
+        roi = threshed[y:y+h+100,x:x+w+50]
+        roismall = cv2.resize(roi,(28,28))
+        cv2.imshow( "Display window", roismall )
+        cv2.waitKey(0); 
+        roismall = roismall.flatten()
+        print(knn.predict(roismall))
+        
